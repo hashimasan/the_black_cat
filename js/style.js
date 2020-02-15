8c0cd0c569379087dd9f1ab84787a58c533ca42f
hashimoto.mana <hashimoto.mana@dtcom.jp>
2020-02-15 14:53:37 +0900

    動かない状態_一旦コミット

--- js/style.js
+++ js/style.js
@@ -1,71 +1,62 @@
 window.onload = function () {
 
-
-};
-
-
-//変数定義
-var navigationOpenFlag = false;
-var navButtonFlag = true;
-var focusFlag = false;
-
-//ハンバーガーメニュー
-$(function () {
-
-    $(document).on('click', '.el_humburger', function () {
-        if (navButtonFlag) {
-            spNavInOut.switch();
-            //一時的にボタンを押せなくする
-            setTimeout(function () {
-                navButtonFlag = true;
-            }, 200);
-            navButtonFlag = false;
-        }
+    $(function () {
+        $('.book1').click(function () {
+            $('.main_book_img').attr('src', 'img/black_cat3.png');
+            $('.book_title').replaceWith(
+                '<p class="book_title">The Black Cat</p>'
+            );
+            $('.about_book').empty();
+            $('.about_book').append(
+                '<span>発売日</span>： 1995/5/28 <br>',
+                '<span>文庫</span>: 292ページ <br>',
+                '<span>出版社</span>: 橋本文庫 <br>',
+                '<span>ISBN-13</span>: 978 - 4087520255 <br>',
+                '<span>定価</span>：540円<br>'
+            );
+        });
+        $('.book2').click(function () {
+            $('.main_book_img').attr('src', 'img/the_gold_bug.png');
+            $('.book_title').replaceWith(
+                '<p class="book_title">黄金虫</p>'
+            );
+            $('.about_book').empty();
+            $('.about_book').append(
+                '<span>発売日</span>： 1996/5/28 <br>',
+                '<span>文庫</span>: 305ページ <br>',
+                '<span>出版社</span>: 橋本文庫 <br>',
+                '<span>ISBN-13</span>: 978 - 4087520256 <br>',
+                '<span>定価</span>：580円<br>'
+            );
+        });
+        $('.book3').click(function () {
+            $('.main_book_img').attr('src', 'img/Morgue.png');
+            $('.book_title').replaceWith(
+                '<p class="book_title">モルグ街の殺人事件</p>'
+            );
+            $('.about_book').empty();
+            $('.about_book').append(
+                '<span>発売日</span>： 1997/5/28<br>',
+                '<span>文庫</span>: 260ページ<br>',
+                '<span>出版社</span>: 橋本文庫<br>',
+                '<span>ISBN-13</span>: 978-4087520257<br>',
+                '<span>定価</span>：500円<br>'
+            );
+        });
+        $('.book4').click(function () {
+            $('.main_book_img').attr('src', 'img/masterpieces.png');
+            $('.book_title').replaceWith(
+                '<p class="book_title">ポー傑作集</p>'
+            );
+            $('.about_book').empty();
+            $('.about_book').append(
+                '<span>発売日</span>： 1998/5/28<br>',
+                '<span>文庫</span>: 452ページ<br>',
+                '<span>出版社</span>: 橋本文庫<br>',
+                '<span>ISBN-13</span>: 978-4087520258<br>',
+                '<span>定価</span>：640円<br>'
+            );
+        });
     });
-    $(document).on('click touchend', function (event) {
-        if (!$(event.target).closest('.bl_header,.el_humburger').length && $('body').hasClass('js_humburgerOpen') && focusFlag) {
-            focusFlag = false;
-            //scrollBlocker(false);
-            spNavInOut.switch();
-        }
-    });
-});
 
-//ナビ開く処理
-function spNavIn() {
-    $('body').removeClass('js_humburgerClose');
-    $('body').addClass('js_humburgerOpen');
-    $('#poe').addClass('none');
-    setTimeout(function () {
-        focusFlag = true;
-    }, 200);
-    setTimeout(function () {
-        navigationOpenFlag = true;
-    }, 200);
-}
-
-//ナビ閉じる処理
-function spNavOut() {
-    $('body').removeClass('js_humburgerOpen');
-    $('body').addClass('js_humburgerClose');
-    $('#poe').removeClass('none');
-    setTimeout(function () {
-        $(".uq_spNavi").removeClass("js_appear");
-        focusFlag = false;
-    }, 200);
-    navigationOpenFlag = false;
-}
-
-//ナビ開閉コントロール
-var spNavInOut = {
-    switch: function () {
-        if ($('body.spNavFreez').length) {
-            return false;
-        }
-        if ($('body').hasClass('js_humburgerOpen')) {
-            spNavOut();
-        } else {
-            spNavIn();
-        }
-    }
 };
